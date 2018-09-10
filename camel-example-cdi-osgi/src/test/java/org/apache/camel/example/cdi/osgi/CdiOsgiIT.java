5decb25c01232d5054169b10681bdf41ac952c6b
Guillaume Nodet <gnodet@gmail.com>
2018-09-10 10:44:48 +0200

    [CAMEL-12818] Remove deprecated stuff

--- camel-example-cdi-osgi/src/test/java/org/apache/camel/example/cdi/osgi/CdiOsgiIT.java
+++ camel-example-cdi-osgi/src/test/java/org/apache/camel/example/cdi/osgi/CdiOsgiIT.java
@@ -14,64 +14,90 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.example.fhir.osgi;
+package org.apache.camel.example.cdi.osgi;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import javax.inject.Inject;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.ServiceStatus;
+import org.apache.camel.api.management.mbean.ManagedRouteMBean;
+import org.apache.karaf.shell.api.console.Session;
+import org.apache.karaf.shell.api.console.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
-import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.ops4j.pax.tinybundles.core.TinyBundles;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.apache.camel.example.cdi.osgi.PaxExamOptions.ACTIVEMQ;
+import static org.apache.camel.example.cdi.osgi.PaxExamOptions.CAMEL_CDI;
+import static org.apache.camel.example.cdi.osgi.PaxExamOptions.CAMEL_COMMANDS;
+import static org.apache.camel.example.cdi.osgi.PaxExamOptions.CAMEL_SJMS;
+import static org.apache.camel.example.cdi.osgi.PaxExamOptions.KARAF;
+import static org.apache.camel.example.cdi.osgi.PaxExamOptions.PAX_CDI_IMPL;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.streamBundle;
 import static org.ops4j.pax.exam.CoreOptions.when;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.debugConfiguration;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-public class FhirOsgiIT {
+public class CdiOsgiIT {
 
     @Inject
     private CamelContext context;
 
+    @Inject
+    private SessionFactory sessionFactory;
+
     @Configuration
     public Option[] config() throws IOException {
         return options(
-            PaxExamOptions.KARAF.option(),
-            PaxExamOptions.CAMEL_FHIR.option(),
+            KARAF.option(),
+            CAMEL_COMMANDS.option(),
+            PAX_CDI_IMPL.option(),
+            CAMEL_CDI.option(),
+            CAMEL_SJMS.option(),
+            ACTIVEMQ.option(),
             streamBundle(
                 TinyBundles.bundle()
                     .read(
                         Files.newInputStream(
                             Paths.get("target")
-                                .resolve("camel-example-fhir-osgi.jar")))
+                                .resolve("camel-example-cdi-osgi.jar")))
                     .build()),
             when(false)
                 .useOptions(
-                    debugConfiguration("5005", true)),
-            CoreOptions.composite(editConfigurationFilePut(
-                "etc/org.apache.camel.example.fhir.osgi.configuration.cfg",
-                new File("org.apache.camel.example.fhir.osgi.configuration.cfg")))
+                    debugConfiguration("5005", true))
         );
     }
 
     @Test
     public void testRouteStatus() {
-        assertNotNull(context);
-        assertEquals("Route status is incorrect!", ServiceStatus.Started, context.getRouteController().getRouteStatus("fhir-example-osgi"));
+        assertThat("Route status is incorrect!",
+            context.getRouteController().getRouteStatus("consumer-route"), equalTo(ServiceStatus.Started));
+    }
+
+    @Test
+    public void testExchangesCompleted() throws Exception {
+        ManagedRouteMBean route = context.getManagedRoute(context.getRoute("consumer-route").getId(), ManagedRouteMBean.class);
+        assertThat("Number of exchanges completed is incorrect!",
+            route.getExchangesCompleted(), equalTo(1L));
+    }
+
+    @Test
+    public void testExecuteCommands() throws Exception {
+        Session session = sessionFactory.create(System.in, System.out, System.err);
+        session.execute("camel:context-list");
+        session.execute("camel:route-list");
+        session.execute("camel:route-info consumer-route");
     }
 }
