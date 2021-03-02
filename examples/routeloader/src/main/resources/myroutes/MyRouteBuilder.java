7c1257e8a737e86c4a483a28ac9a603aa6ebf482
Claus Ibsen <claus.ibsen@gmail.com>
2021-03-02 17:48:34 +0100

    Polished

--- examples/routeloader/src/main/resources/myroutes/MyRouteBuilder.java
+++ examples/routeloader/src/main/resources/myroutes/MyRouteBuilder.java
@@ -6,7 +6,7 @@ public class MyRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer:foo?period=2s")
+        from("timer:java?period=2s")
             .setBody(method(MyRouteBuilder.class, "randomNumber"))
             .log("Random number ${body}");
     }
