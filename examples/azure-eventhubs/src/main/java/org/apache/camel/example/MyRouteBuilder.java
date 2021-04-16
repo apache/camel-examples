5892c8382e428852f9c4c7cd92d9fadd2fcda612
Andrea Cosentino <ancosen@gmail.com>
2021-04-16 07:50:11 +0200

    Added Azure Eventhubs example

---
+++ examples/azure-eventhubs/src/main/java/org/apache/camel/example/MyRouteBuilder.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.example;
+
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+import org.apache.camel.component.azure.eventhubs.EventHubsConstants;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * To use the endpoint DSL then we must extend EndpointRouteBuilder instead of RouteBuilder
+ */
+public class MyRouteBuilder extends EndpointRouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from(azureEventhubs("{{namespaceName}}/{{eventhubName}}").sharedAccessKey("{{sharedAccessKey}}").sharedAccessName("{{sharedAccessName}}").blobAccessKey("{{blobAccessKey}}").blobAccountName("{{blobAccountName}}").blobContainerName("{{blobContainerName}}"))
+                .log("The content is ${body}");
+
+        from(timer("tick").period(1000)).setBody(constant("ciao"))
+                .to(azureEventhubs("{{namespaceName}}/{{eventhubName}}").sharedAccessName("{{sharedAccessName}}").sharedAccessKey("{{sharedAccessKey}}"));
+    }
+}
