7c13a5d5b270385b05df8ecd30e1649f085b4149
Claus Ibsen <claus.ibsen@gmail.com>
2021-11-18 08:57:38 +0100

    CAMEL-16656: camel-core - ResourceReloader SPI - Allow to reload on changes

--- examples/main-xml/src/main/java/org/apache/camel/example/MyApplication.java
+++ examples/main-xml/src/main/java/org/apache/camel/example/MyApplication.java
@@ -34,6 +34,10 @@ public final class MyApplication {
         main.configure().addConfigurationClass(MyConfiguration.class);
         // and add all the XML routes
         main.configure().withRoutesIncludePattern("routes/*.xml");
+        // turn on reloading routes on code-changes
+        main.configure().withRoutesReloadEnabled(true);
+        main.configure().withRoutesReloadDirectory("src/main/resources");
+        main.configure().withRoutesReloadPattern("routes/*.xml");
 
         // now keep the application running until the JVM is terminated (ctrl + c or sigterm)
         main.run(args);
