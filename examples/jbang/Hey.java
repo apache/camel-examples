c9d2075c70f5489b769ed5259fc5628bdac7fbc1
Claus Ibsen <claus.ibsen@gmail.com>
2022-02-15 18:42:29 +0100

    Polish example

--- examples/jbang/Hey.java
+++ examples/jbang/Hey.java
@@ -1,11 +1,12 @@
-import org.apache.camel.builder.RouteBuilder;
+// use modeline to configure properties directly in the same source file
+// camel-k: language=java name=Cool property=period=1000
 
-public class Hey extends RouteBuilder {
+public class Hey extends org.apache.camel.builder.RouteBuilder {
 
   @Override
   public void configure() throws Exception {
       // Write your routes here, for example:
-      from("timer:java?period=1000")
+      from("timer:java?period={{period}}")
         .routeId("java")
         .process(e -> {
            e.getMessage().setBody("Hello from Camel");
