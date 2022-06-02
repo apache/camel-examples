89b31c7670fdb72e18a1d62d1881bb177abb23a2
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-06-02 17:00:51 +0200

    CAMEL-18127: added Resume API v2 example for AWS Kinesis

---
+++ examples/resume-api/resume-api-aws2-kinesis/README.md
@@ -0,0 +1,73 @@
+Resume API Example: AWS Kinesis
+=========================
+
+This example shows how to use the Resume API for consuming AWS Kinesis data streams. 
+
+It uses LocalStack to simulate the AWS Kinesis instance. 
+
+First the demo load 500 records into the data stream. Then, it consumes then in batches of 50 messages. It starts each batch at the point of last consumption. The offsets are stored in a Kafka topic.
+
+*Note*: this demo runs in a container. Although it is possible to run it outside a container, doing so requires additional infrastructure. Therefore, it's not extensively covered by the documentation.
+
+Building the demo
+===
+
+To build the demo and the containers:
+
+```shell
+mvn clean package && docker-compose build
+```
+
+Run
+===
+
+To run the demo:
+
+```shell
+docker-compose up -d && docker-compose logs --no-log-prefix -f example ; docker-compose down
+```
+
+Advanced / Manual
+===
+
+Launch LocalStack
+====
+
+```
+docker run --rm -e SERVICES=kinesis -e DEBUG=1 -e LS_LOG=trace -it -p 4566:4566 localstack/localstack:latest
+```
+
+Data Load
+======
+
+To load the data run: 
+
+```shell
+  java -Dresume.type=kafka \
+    -Dresume.type.kafka.topic=aws-kinesis-offsets \
+    -Dresume.action=load \
+    -Daws.kinesis.streamName=sample-stream \
+    -Daws-service.kinesis.instance.type=remote \
+    -Daws.host=kinesis:4566 \
+    -Daws.access.key=accesskey \
+    -Daws.secret.key=secretkey \
+    -Daws.cborEnabled=false \
+    -jar /deployments/example.jar
+```
+
+Run the Example
+======
+
+```shell
+java -Dresume.type=kafka \
+           -Dresume.type.kafka.topic=aws-kinesis-offsets \
+           -Dbootstrap.address=REPLACE-WITH-KAFKA-HOST:9092 \
+           -Daws.kinesis.streamName=sample-stream \
+           -Daws-service.kinesis.instance.type=remote \
+           -Daws.host=REPLACE-WITH-KINESIS-HOST:4566 \
+           -Daws.access.key=accesskey \
+           -Daws.secret.key=secretkey \
+           -Daws.cborEnabled=false \
+           -Dbatch.size=50 \
+           -jar /deployments/example.jar
+```
---
+++ examples/resume-api/resume-api-aws2-kinesis/src/main/java/org/apache/camel/example/resume/aws/kinesis/main/KinesisRoute.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.example.resume.aws.kinesis.main;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.aws2.kinesis.Kinesis2Constants;
+import org.apache.camel.resume.ResumeStrategy;
+import org.apache.camel.resume.cache.ResumeCache;
+import org.apache.camel.support.resume.Resumables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.kinesis.KinesisClient;
+
+public class KinesisRoute extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(KinesisRoute.class);
+
+    private final String streamName;
+    private final ResumeStrategy resumeStrategy;
+    private final ResumeCache<String> resumeCache;
+    private final KinesisClient client;
+    private final CountDownLatch latch;
+
+    public KinesisRoute(String streamName, ResumeStrategy resumeStrategy, ResumeCache<String> resumeCache, KinesisClient client, CountDownLatch latch) {
+        this.streamName = streamName;
+        this.resumeStrategy = resumeStrategy;
+        this.resumeCache = resumeCache;
+        this.client = client;
+        this.latch = latch;
+    }
+
+    private void addResumeOffset(Exchange exchange) {
+        String sequenceNumber = exchange.getMessage().getHeader(Kinesis2Constants.SEQUENCE_NUMBER, String.class);
+        exchange.getMessage().setHeader(Exchange.OFFSET, Resumables.of(streamName, sequenceNumber));
+
+        String body = exchange.getMessage().getBody(String.class);
+        LOG.warn("Processing: {} with sequence number {}", body, sequenceNumber);
+        latch.countDown();
+    }
+
+    @Override
+    public void configure() {
+        bindToRegistry("testResumeStrategy", resumeStrategy);
+        bindToRegistry("resumeCache", resumeCache);
+        bindToRegistry("amazonKinesisClient", client);
+
+        String kinesisEndpointUri = "aws2-kinesis://%s?amazonKinesisClient=#amazonKinesisClient";
+
+        fromF(kinesisEndpointUri, streamName)
+                .process(this::addResumeOffset)
+                .resumable("testResumeStrategy");
+    }
+}
