831fe3d03eac0fe8f47342d841f85f704c5db3ae
Claus Ibsen <claus.ibsen@gmail.com>
2026-05-08 13:22:03 +0200

    CAMEL-23400: camel-test - Add route diagram dumper

--- main/src/test/java/org/apache/camel/example/MainDiagramTest.java
+++ main/src/test/java/org/apache/camel/example/MainDiagramTest.java
@@ -16,31 +16,18 @@
  */
 package org.apache.camel.example;
 
-import java.util.concurrent.TimeUnit;
-
-import org.apache.camel.BeanInject;
-import org.apache.camel.CamelContext;
-import org.apache.camel.builder.NotifyBuilder;
 import org.apache.camel.test.main.junit6.CamelMainTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 /**
- * A unit test using the annotation based approach checking that Camel supports binding via annotations.
+ * To dump route diagram only once
  */
-@CamelMainTest(mainClass = MyApplication.class)
-class MainWithAnnotationTest {
-
-    @BeanInject
-    CamelContext context;
+@CamelMainTest(mainClass = MyApplication.class, dumpRouteDiagramFolder = "doc")
+class MainDiagramTest {
 
     @Test
-    void should_support_binding_via_annotations() {
-        NotifyBuilder notify = new NotifyBuilder(context)
-            .whenCompleted(1).whenBodiesDone("Hello how are you?").create();
-        assertTrue(
-            notify.matches(20, TimeUnit.SECONDS), "1 message should be completed"
-        );
+    void empty() {
+        // empty test method
     }
+
 }
